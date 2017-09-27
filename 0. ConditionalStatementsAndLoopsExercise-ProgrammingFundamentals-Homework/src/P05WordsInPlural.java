5fa8a1e73091f751879044c121091e035cd52599
Asen Bonkov <asenbonkov@abv.bg>
2017-09-27 23:42:15 +0300

    up

---
+++ 0. ConditionalStatementsAndLoopsExercise-ProgrammingFundamentals-Homework/src/P05WordsInPlural.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P05WordsInPlural {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String word = scanner.nextLine();
+
+        if (String.)
+    }
+}
---
+++ 0. ConditionalStatementsAndLoopsExercise-ProgrammingFundamentals-Homework/src/P115DifferentNumbers.java
@@ -0,0 +1,2 @@
+public class P115DifferentNumbers {
+}
